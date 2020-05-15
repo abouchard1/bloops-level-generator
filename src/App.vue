0efc0c9319be487da0a9ca7c65e45feb5f18187d
Clément <clemvnt@gmail.com>
2020-05-15 14:32:54 +0200

    add world field

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div>
     <h1>Bloops - Level generator</h1>
-    <select v-model="selectedTypeIndex">
-      <option v-for="(type, index) in types" :value="index" :key="type.name">{{type.name}}</option>
-    </select>
-    <button @click="clear()">Clear</button>
+    <div class="mb-2">
+      <label style="display: block">World</label>
+      <input type="number" v-model="worldNumber" min="1" />
+    </div>
+    <div class="mb-2">
+      <label style="display: block">Block type</label>
+      <select v-model="selectedTypeIndex">
+        <option
+          v-for="(type, index) in types"
+          :value="index"
+          :key="type.name"
+          >{{ type.name }}</option
+        >
+      </select>
+    </div>
+    <div class="mb-2">
+      <button @click="clear()">Clear</button>
+    </div>
     <div class="grid">
       <div
         v-for="item in items"
         :key="item.id"
         class="item"
-        :style="{backgroundColor: item.active && item.selectedTypeIndex > -1 ? types[item.selectedTypeIndex].color : 'white'}"
+        :style="{
+          backgroundColor:
+            item.active && item.selectedTypeIndex > -1
+              ? types[item.selectedTypeIndex].color
+              : 'white'
+        }"
         @click="toggle(item)"
-      >{{item.id}}</div>
+      >
+        {{ item.id }}
+      </div>
     </div>
-    <button @click="generate()">Generate</button>
+    <div class="m-2">
+      <button @click="generate()">Generate</button>
+    </div>
     <div>
-      <textarea v-model="json" cols="30" rows="10"></textarea>
+      <textarea v-model="json" cols="120" rows="10"></textarea>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/composition-api";
+import { ref } from '@vue/composition-api'
 
 export default {
   setup() {
@@ -34,66 +57,77 @@ export default {
         x: -9 + ((i + 1) % 17 === 0 ? 17 : (i + 1) % 17),
         y: -(-5 + Math.ceil((i + 1) / 17))
       }))
-    );
+    )
     const types = [
-      { name: "wall", color: "burlywood" },
-      { name: "character", color: "red" },
-      { name: "portal", color: "green" }
-    ];
-    const selectedTypeIndex = ref(0);
-    const json = ref("");
+      { name: 'wall', color: 'burlywood' },
+      { name: 'character', color: 'red' },
+      { name: 'portal', color: 'green' }
+    ]
+    const worldNumber = ref(1)
+    const selectedTypeIndex = ref(0)
+    const json = ref('')
 
     function clear() {
       items.value.forEach(i => {
-        i.active = false;
-        i.selectedTypeIndex = -1;
-      });
+        i.active = false
+        i.selectedTypeIndex = -1
+      })
     }
 
     function toggle(item) {
-      item.active = !item.active;
-      item.selectedTypeIndex = item.active ? selectedTypeIndex.value : -1;
+      item.active = !item.active
+      item.selectedTypeIndex = item.active ? selectedTypeIndex.value : -1
     }
 
     function generate() {
       const character = items.value.find(item =>
         item.selectedTypeIndex > -1
-          ? types[item.selectedTypeIndex].name === "character"
+          ? types[item.selectedTypeIndex].name === 'character'
           : false
-      );
+      )
       const portal = items.value.find(item =>
         item.selectedTypeIndex > -1
-          ? types[item.selectedTypeIndex].name === "portal"
+          ? types[item.selectedTypeIndex].name === 'portal'
           : false
-      );
+      )
       const blocs = items.value.filter(item =>
         item.selectedTypeIndex > -1
-          ? types[item.selectedTypeIndex].name === "wall"
+          ? types[item.selectedTypeIndex].name === 'wall'
           : false
-      );
+      )
       json.value = JSON.stringify({
-        name: "Niveau",
+        name: 'Niveau',
+        world: worldNumber.value,
         character: character && {
-          type: "PERSONNAGE",
+          type: 'PERSONNAGE',
           pos_x: character.x,
           pos_y: character.y
         },
         portal: portal && {
-          type: "PORTAIL",
+          type: 'PORTAIL',
           pos_x: portal.x,
           pos_y: portal.y
         },
         blocs: blocs.map(i => ({
           pos_x: i.x,
           pos_y: i.y,
-          type: "wall"
+          type: 'wall'
         }))
-      });
+      })
     }
 
-    return { types, selectedTypeIndex, items, clear, toggle, generate, json };
+    return {
+      types,
+      worldNumber,
+      selectedTypeIndex,
+      items,
+      clear,
+      toggle,
+      generate,
+      json
+    }
   }
-};
+}
 </script>
 
 <style>
@@ -107,5 +141,12 @@ export default {
   border: 1px solid black;
   cursor: pointer;
 }
-</style>
 
+.m-2 {
+  margin: 20px 0;
+}
+
+.mb-2 {
+  margin-bottom: 20px;
+}
+</style>
